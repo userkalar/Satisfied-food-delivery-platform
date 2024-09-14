e173317086d684e0574799a14116ebdedc4bc41e
test <2662154762@qq.com>
2024-09-14 16:38:50 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,11 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     /**
-     * 统计指定时间区间内的数据
+     * 统计指定时间区间内的营业额数据
      *
      * @param begin
      * @param end
@@ -55,16 +59,16 @@ public class ReportServiceImpl implements ReportService {
         for (LocalDate date : datalist) {
             //查询data日期对应的营业额数据，营业额是状态为“已完成”的订单金额合计
             LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
-            LocalDateTime endTime = LocalDateTime.of(date,LocalTime.MAX);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
 
 
             // select sum(amount) from orders where order_time > beginTime and order_time < endTime and status = 5
-            Map map=new HashMap();
-            map.put("begin",beginTime);
-            map.put("end",endTime);
+            Map map = new HashMap();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
             map.put("status", Orders.COMPLETED);
             Double turnover = orderMapper.sumByMap(map);
-            turnover = turnover == null ? 0.0 :turnover;
+            turnover = turnover == null ? 0.0 : turnover;
             turnoverList.add(turnover);
         }
 
@@ -73,7 +77,58 @@ public class ReportServiceImpl implements ReportService {
         return TurnoverReportVO
                 .builder()
                 .dateList(StringUtils.join(datalist, ","))
-                .turnoverList(StringUtils.join(turnoverList,","))
+                .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+
+    /**
+     * 统计指定时间区间内的用户数据
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+
+        //存放从begin到end之间的每天对应的日期
+        List<LocalDate> dateList = new ArrayList<>();
+
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+
+        }
+
+        //存放每天的新增用户数量  select count(id) from user where create_time < beginTime and create_time > endTime and status = 5
+        List<Integer> newUserList = new ArrayList<>();
+        //存放每天的总用户数量    select count(id) from user where create_time < beginTime
+        List<Integer> totalUserList = new ArrayList<>();
+
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+
+            Map map = new HashMap();
+            map.put("end", endTime);
+            //总用户数量
+            Integer totalUser = userMapper.countByMap(map);
+            map.put("begin", beginTime);
+            Integer newUser = userMapper.countByMap(map);
+
+            totalUserList.add(totalUser);
+            newUserList.add(newUser);
+
+        }
+
+        //封装结果数据
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
                 .build();
     }
 }
